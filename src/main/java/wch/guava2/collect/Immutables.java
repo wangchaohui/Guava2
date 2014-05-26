87c9d49d34c25aa8b4ecc2a35e231dfda2d4a190
Wang Chaohui <CH.WANG@ntu.edu.sg>
2014-05-26 22:16:00 +0800

    Immutables 2

--- src/main/java/wch/guava2/collect/Immutables.java
+++ src/main/java/wch/guava2/collect/Immutables.java
@@ -3,7 +3,9 @@ package wch.guava2.collect;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
+import java.util.List;
 import java.util.Set;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
 import wch.guava2.collectors.Collectors2;
@@ -15,10 +17,27 @@ import wch.guava2.collectors.Collectors2;
  */
 public class Immutables {
 
-  public static <K, V> ImmutableMap<K, V> createImmutableMap(
+  public static <K, V> ImmutableMap<K, V> buildImmutableMap(
       Consumer<ImmutableMap.Builder<K, V>> consumer) {
     ImmutableMap.Builder<K, V> builder = ImmutableMap.builder();
     consumer.accept(builder);
+    return builder.build();
+  }
+
+  public static <T> ImmutableList<T> buildImmutableList(
+      Consumer<ImmutableList.Builder<T>> consumer) {
+    ImmutableList.Builder<T> builder = ImmutableList.builder();
+    consumer.accept(builder);
+    return builder.build();
+  }
+
+  public static <T, R> ImmutableList<R> toImmutableList(List<T> list,
+                                                        BiFunction<Integer, T, R> consumer) {
+    ImmutableList.Builder<R> builder = ImmutableList.builder();
+    int n = list.size();
+    for (int i = 0; i < n; i++) {
+      builder.add(consumer.apply(i, list.get(i)));
+    }
     return builder.build();
   }
 
